98b199d3e5bdc60963b2db0461ee9317f5a5d10a
Hugo Oyola <hugo.ham@outlook.com>
2024-04-01 12:13:01 -0500

    Actualizar selectores y agregar EventListeners y función de datosCitas en app.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,29 @@
 // Selectores
-const pacienteInput = document.querySelector('#paciente');
-const propietarioInput = document.querySelector('#propietario');
-const emailInput = document.querySelector('#email');
-const fechaInput = document.querySelector('#fecha');
-const sintomasInput = document.querySelector('#sintomas');
+const pacienteInput = document.querySelector("#paciente");
+const propietarioInput = document.querySelector("#propietario");
+const emailInput = document.querySelector("#email");
+const fechaInput = document.querySelector("#fecha");
+const sintomasInput = document.querySelector("#sintomas");
 
 // Objeto de cita
 const citaObj = {
-    paciente: '',
-    propietario: '',
-    email: '',
-    fecha: '',
-    sintomas: ''
+  paciente: "",
+  propietario: "",
+  email: "",
+  fecha: "",
+  sintomas: "",
+};
+
+// EventListeners
+pacienteInput.addEventListener("change", datosCitas);
+propietarioInput.addEventListener("change", datosCitas);
+emailInput.addEventListener("change", datosCitas);
+fechaInput.addEventListener("change", datosCitas);
+sintomasInput.addEventListener("change", datosCitas);
+
+// Funciones
+function datosCitas(e) {
+  citaObj[e.target.name] = e.target.value;
+  console.log(citaObj);
 }
+
