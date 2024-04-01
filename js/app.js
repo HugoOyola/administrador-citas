24e904da367a435bff130b151083c50bc2607676
Hugo Oyola <hugo.ham@outlook.com>
2024-04-01 18:54:57 -0500

    Agregada función para mostrar notificaciones en la aplicación

--- js/app.js
+++ js/app.js
@@ -39,7 +39,7 @@ function submitCita(e) {
       texto: "Todos los campos son obligatorios",
       tipo: "error"
     });
-    console.log(notificacion);
+    notificacion.mostrar();
     return;
   }
 }
@@ -48,5 +48,29 @@ class Notificacion {
   constructor({texto, tipo}) {
     this.texto = texto;
     this.tipo = tipo;
+  }
+
+  mostrar(){
+    // Crear notificación
+    const alerta = document.createElement("DIV");
+    alerta.classList.add("text-center", "w-full", "p-3", "text-white", "my-5", "alert", "uppercase", "font-bold", "text-sm");
+
+    // Eliminar alertas previas
+    const alertaPrevia = document.querySelector(".alert");
+    alertaPrevia?.remove()
+
+    // Si es tipo error, agregar una clase
+    this.tipo === "error" ? alerta.classList.add("bg-red-500") : alerta.classList.add("bg-green-500");
+
+    // Mensaje de error
+    alerta.textContent = this.texto;
+
+    // Agregar al DOM
+    formulario.parentElement.insertBefore(alerta, formulario);
+
+    // Eliminar la alerta después de 5 segundos
+    setTimeout(() => {
+      alerta.remove();
+    }, 3000);
   }
 }
